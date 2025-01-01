ce180c3274facb13501dcf46cd2b4e157058b20e
DESKTOP-66L2F1C\박경륜 <parkgr.illuni@gmail.com>
2025-01-01 20:36:02 +0900

    에디터 코드 설정

--- Assets/Scripts/Utils/EditorStartInit.cs
+++ Assets/Scripts/Utils/EditorStartInit.cs
@@ -1,13 +1,17 @@
 using UnityEditor;
 using UnityEditor.SceneManagement;
 
+# if UNITY_EDITOR
 [InitializeOnLoad]
+#endif
 public class EditorStartInit
 {
+# if UNITY_EDITOR
     static EditorStartInit()
     {
         var pathOfFirstScene = EditorBuildSettings.scenes[0].path; // 씬 번호를 넣어주자.
         var sceneAsset = AssetDatabase.LoadAssetAtPath<SceneAsset>(pathOfFirstScene);
         EditorSceneManager.playModeStartScene = sceneAsset;
     }
+#endif
 }
